df2cf5dd02bcf6b0ca303ba63e028b074c487e0a
Ghezal Ahmad Jan Zia <ghezalahmad.zia@outlook.com>
2025-07-07 21:08:07 +0200

    SLAMD Setup set

--- setup.py
+++ setup.py
@@ -1,31 +1,39 @@
 from setuptools import setup
+import os
 import glob
 
 APP = ['run_slamd.py']
 
-# Collect template files
-template_files = glob.glob('slamd/templates/*.html')
-static_files = glob.glob('slamd/static/*')
-lib_files = glob.glob('slamd/libs/*')
-
-DATA_FILES = [
-    ('slamd/templates', template_files),
-    ('slamd/static', static_files),
-    ('slamd/libs', lib_files)
+# SLAMD has templates and static files in multiple submodules, so we include them all.
+RESOURCE_DIRS = [
+    'slamd/templates',
+    'slamd/static',
+    'slamd/libs',
+    'slamd/common',
+    'slamd/discovery/templates',
+    'slamd/discovery/static',
+    'slamd/formulations/templates',
+    'slamd/formulations/static',
+    'slamd/materials/templates',
+    'slamd/materials/static',
+    'slamd/design_assistant/templates',
+    'slamd/design_assistant/static'
 ]
+
+# Flatten resource folder list (just folder paths, py2app will include content recursively)
+RESOURCES = [os.path.join(*d.split('/')) for d in RESOURCE_DIRS]
 
 OPTIONS = {
     'argv_emulation': True,
-    'iconfile': 'slamd.icns',
+    'iconfile': 'slamd.icns',  # optional, only if you want a custom app icon
+    'includes': ['encodings', 'idna.idnadata'],
     'packages': ['flask', 'jinja2', 'py4j', 'lolopy', 'werkzeug', 'pandas', 'numpy', 'scipy', 'sklearn', 'plotly'],
     'excludes': ['PyQt6', 'PyQt5', 'PyInstaller'],
+    'resources': RESOURCES,
 }
-
-
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
